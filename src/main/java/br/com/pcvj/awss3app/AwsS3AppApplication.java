e249242f16f8e8230e7771abd59b779dde86d45c
Paulo Vieira <80427803+vieirajunior-90@users.noreply.github.com>
2023-11-18 10:46:49 -0300

    Projeto iniciado

---
+++ src/main/java/br/com/pcvj/awss3app/AwsS3AppApplication.java
@@ -0,0 +1,13 @@
+package br.com.pcvj.awss3app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsS3AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsS3AppApplication.class, args);
+    }
+
+}
